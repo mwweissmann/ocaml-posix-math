c3c8ec12d4961b96c52fbe2b3c09ab2944590d39
Markus Weissmann <markus.weissmann@in.tum.de>
2015-09-27 19:25:15 +0200

    add assertions to test

--- test/test.ml
+++ test/test.ml
@@ -4,7 +4,9 @@ let x, y = 11.5, 12.5
 
 let _ =
   Printf.printf "%f -> %f\n" x (rint x); (* 12.0 *)
+  assert((rint 11.5) = 12.0);
   Printf.printf "%f -> %f\n" y (rint y); (* 12.0 *)
+  assert((rint 12.5) = 12.0);
 
   print_endline "done"
 
@@ -13,7 +15,9 @@ let _ =
   fesetround Fround.fe_downward;
   Printf.printf "%f\n" (1.0 /. 0.0);
   Printf.printf "%f -> %f\n" x (rint x); (* 11.0 *)
+  assert((rint 11.5) = 11.0);
   Printf.printf "%f -> %f\n" y (rint y); (* 12.0 *)
+  assert((rint 12.5) = 12.0);
 
   print_endline "done"
 
