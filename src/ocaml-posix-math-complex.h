230aeefac291d3bd130a3b3f4b0b7d68cc437c04
Markus W. Weissmann <mail@mweissmann.de>
2016-05-01 23:12:42 +0200

    fix conversion of OCaml Complex.t to C 'double complex'

--- src/ocaml-posix-math-complex.h
+++ src/ocaml-posix-math-complex.h
@@ -4,7 +4,7 @@
 #include <complex.h>
 #include <caml/mlvalues.h>
 
-#define Complex_val(v) ((double complex) (Double_field(v, 0) + I * Double_field(v, 0)))
+#define Complex_val(v) ((double complex) (Double_field(v, 0) + Double_field(v, 1) * _Complex_I))
 CAMLextern value caml_copy_complex(double complex c);
 
 #endif
